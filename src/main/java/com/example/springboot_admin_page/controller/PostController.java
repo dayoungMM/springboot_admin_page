548bb91320d7092f16f6b2c2521ae6f09f2e302f
dayoungMM <dalbeetm@gmail.com>
2021-07-11 17:54:48 +0900

    04.POST

---
+++ src/main/java/com/example/springboot_admin_page/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.example.springboot_admin_page.controller;
+
+import com.example.springboot_admin_page.model.SearchParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+
+    @PostMapping("/postMethod")
+    public SearchParam postMethod(@RequestBody SearchParam searchParam){
+        return searchParam;
+    }
+
+}
