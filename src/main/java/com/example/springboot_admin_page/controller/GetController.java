bffbedb1cc6cc0fe08b82248773de857ec2c3053
dayoungMM <dalbeetm@gmail.com>
2021-07-11 17:55:48 +0900

    refactor: rename function name

--- src/main/java/com/example/springboot_admin_page/controller/GetController.java
+++ src/main/java/com/example/springboot_admin_page/controller/GetController.java
@@ -4,25 +4,23 @@ import com.example.springboot_admin_page.model.SearchParam;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api") // localhost:8080/api
+@RequestMapping("/api")
 public class GetController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/getMethod") // localhost:8080/api/getMethod
-    public String getReqeust() {
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod")
+    public String getRequest() {
 
         return "Hi getMethod";
     }
 
-    @GetMapping("/getParameter") // localhost:8080/api/getParameter?id=1234&password=abcd
+    @GetMapping("/getParameter")
     public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd) {
-        String password = "bbbb";
 
         System.out.println("id : " + id);
         System.out.println("password : " + pwd);
         return id + pwd;
     }
 
-    // localhost:8080/api/getMultiParameter?account=abcd&email=study@gmail.com&page=10
     @GetMapping("/getMultiParameter")
     public SearchParam getMultiParameter(SearchParam searchParam){
         System.out.println(searchParam.getAccount());
