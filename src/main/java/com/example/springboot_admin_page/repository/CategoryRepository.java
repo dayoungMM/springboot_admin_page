2b3bee71128cb64f57a2c509b84922cca8f22c33
dayoungMM <dalbeetm@gmail.com>
2021-07-11 21:46:00 +0900

    19.Entity 및 Repository 설정 -2

--- src/main/java/com/example/springboot_admin_page/repository/CategoryRepository.java
+++ src/main/java/com/example/springboot_admin_page/repository/CategoryRepository.java
@@ -1,15 +1,9 @@
 package com.example.springboot_admin_page.repository;
 
-import com.example.springboot_admin_page.model.entity.User;
+import com.example.springboot_admin_page.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
-
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- src/main/java/com/example/springboot_admin_page/repository/PartnerRepository.java
+++ src/main/java/com/example/springboot_admin_page/repository/PartnerRepository.java
@@ -1,15 +1,9 @@
 package com.example.springboot_admin_page.repository;
 
-import com.example.springboot_admin_page.model.entity.User;
+import com.example.springboot_admin_page.model.entity.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-
-
-
+public interface PartnerRepository extends JpaRepository<Partner, Long> {
 }
