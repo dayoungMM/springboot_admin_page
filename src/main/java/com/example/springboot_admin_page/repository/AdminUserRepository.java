5c2855cb475ca4ff3dbb32f07a511853d8a295b5
dayoungMM <dalbeetm@gmail.com>
2021-07-23 14:56:09 +0900

    23.order group admin user 테이블 테스트

--- src/main/java/com/example/springboot_admin_page/repository/AdminUserRepository.java
+++ src/main/java/com/example/springboot_admin_page/repository/AdminUserRepository.java
@@ -1,9 +1,9 @@
 package com.example.springboot_admin_page.repository;
 
-import com.example.springboot_admin_page.model.entity.AdminUSer;
+import com.example.springboot_admin_page.model.entity.AdminUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AdminUserRepository extends JpaRepository<AdminUSer, Long> {
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
 }
