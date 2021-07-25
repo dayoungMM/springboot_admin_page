1cdc4f909e093d19bcf2a3a506dbbad88c2cb8c2
dayoungMM <dalbeetm@gmail.com>
2021-07-25 21:59:51 +0900

    10-1 샘플데이터 생성하기

--- src/test/java/com/example/springboot_admin_page/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/springboot_admin_page/repository/ItemRepositoryTest.java
@@ -3,7 +3,9 @@ package com.example.springboot_admin_page.repository;
 
 import com.example.springboot_admin_page.SpringbootAdminPageApplication;
 import com.example.springboot_admin_page.model.entity.Item;
+import com.example.springboot_admin_page.model.enumclass.ItemStatus;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,35 +16,38 @@ import java.util.Optional;
 @SpringBootTest
 public class ItemRepositoryTest extends SpringbootAdminPageApplication {
 
-    @Autowired
-    private ItemRepository itemRepository;
+        @Autowired
+        private ItemRepository itemRepository;
 
-    @Test
-    public void create(){
-        Item item = new Item();
-        item.setStatus("UNREGISTERED");
-        item.setName("삼성노트북");
-        item.setTitle("삼성노트북 a100");
-        item.setContent("2021년형");
-        item.setPrice(900000);
-        item.setBrandName("삼성");
-        item.setRegisteredAt(LocalDateTime.now());
-        item.setCreatedAt(LocalDateTime.now());
-        item.setCreatedBy("Partner01");
-//        item.setPartnerId(1L);
+        @Test
+        public void create() {
+            Item item = new Item();
+            item.setStatus(ItemStatus.UNREGISTERED);
+            item.setName("삼성 노트북");
+            item.setTitle("삼성 노트북 A100");
+            item.setContent("2019년형 삼성 노트북 입니다.");
+//    item.setPrice(900000);
+            item.setBrandName("삼성");
+            item.setRegisteredAt(LocalDateTime.now());
+            item.setCreatedAt(LocalDateTime.now());
+            item.setCreatedBy("Partner01");
+//    item.setPartnerId(1L);
 
-        Item newItem = itemRepository.save(item);
-        Assert.assertNotNull(newItem);
+
+            Item newItem = itemRepository.save(item);
+            Assertions.assertNotNull(newItem);
+        }
+
+        @Test
+        public void read() {
+            Long id = 1L;
+
+            Optional<Item> item = itemRepository.findById(id);
+
+            Assertions.assertTrue(item.isPresent());
+
+        }
+
 
     }
 
-    @Test
-    public void read(){
-        Long id = 1L;
-
-        Optional<Item> item = itemRepository.findById(id);
-
-        Assert.assertTrue(item.isPresent());
-
-    }
-}
