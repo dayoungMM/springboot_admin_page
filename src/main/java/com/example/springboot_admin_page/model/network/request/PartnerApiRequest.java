ed3c8420468b5d93e6807e38e79ff3f156d81895
dayoungMM <dalbeetm@gmail.com>
2021-07-25 21:31:19 +0900

    9-06. 추상화 코드 적용해보기

---
+++ src/main/java/com/example/springboot_admin_page/model/network/request/PartnerApiRequest.java
@@ -0,0 +1,34 @@
+package com.example.springboot_admin_page.model.network.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PartnerApiRequest {
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private String callCenter;
+
+    private String partnerNumber;
+
+    private String businessNumber;
+
+    private String ceoName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private Long categoryId;
+}
---
+++ src/main/java/com/example/springboot_admin_page/model/network/response/PartnerApiResponse.java
@@ -0,0 +1,35 @@
+package com.example.springboot_admin_page.model.network.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PartnerApiResponse {
+    private Long id;
+
+    private String name;
+
+
+    private String address;
+
+    private String callCenter;
+
+    private String partnerNumber;
+
+    private String businessNumber;
+
+    private String ceoName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+    
+    private Long categoryId;
+}
