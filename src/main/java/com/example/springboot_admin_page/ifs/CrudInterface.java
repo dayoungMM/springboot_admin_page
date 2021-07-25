f178e7bd3856434cb5f0f3352b8fa3c57d3b0cab
dayoungMM <dalbeetm@gmail.com>
2021-07-25 17:26:16 +0900

    8-1-02 사용자 API CREATE -2

--- src/main/java/com/example/springboot_admin_page/ifs/CrudInterface.java
+++ src/main/java/com/example/springboot_admin_page/ifs/CrudInterface.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public interface CrudInterface<Req,Res> {
 
-    Header<Res> create(Req request);
+    Header<Res> create(Header<Req> request);
 
     Header<Res> read(Long id);
 
-    Header<Res> update(Req request);
+    Header<Res> update(Header<Req> request);
 
     Header delete(Long id);
 
--- src/main/java/com/example/springboot_admin_page/service/UserApiLogicService.java
+++ src/main/java/com/example/springboot_admin_page/service/UserApiLogicService.java
@@ -1,12 +1,15 @@
 package com.example.springboot_admin_page.service;
 
 import com.example.springboot_admin_page.ifs.CrudInterface;
+import com.example.springboot_admin_page.model.entity.User;
 import com.example.springboot_admin_page.model.network.Header;
 import com.example.springboot_admin_page.model.network.request.UserApiRequest;
 import com.example.springboot_admin_page.model.network.response.UserApiResponse;
 import com.example.springboot_admin_page.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UserApiLogicService implements CrudInterface<UserApiRequest, UserApiResponse> {
@@ -18,8 +21,22 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     // user 생성
     // 생성된 데이터를 가지고 UserApiResponse를 만들어서 return
     @Override
-    public Header<UserApiResponse> create(UserApiRequest request) {
-        return null;
+    public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+
+        UserApiRequest userApiRequest = request.getData();
+
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        User newUser = userRepository.save(user);
+
+        return response(newUser);
     }
 
     @Override
@@ -28,12 +45,27 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     }
 
     @Override
-    public Header<UserApiResponse> update(UserApiRequest request) {
+    public Header<UserApiResponse> update(Header<UserApiRequest> request) {
         return null;
     }
 
     @Override
     public Header delete(Long id) {
         return null;
+    }
+
+
+    private Header<UserApiResponse> response(User user){
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .phoneNumber(user.getEmail())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        return Header.OK(userApiResponse);
     }
 }
